e6d3d630f096b9f779e3086060de22210be8fa88
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-15 22:11:09 -0300

    feat: new Client class

--- src/main/java/management/insight_api/model/Cliente.java
+++ src/main/java/management/insight_api/model/Cliente.java
@@ -1,4 +1,20 @@
 package management.insight_api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_clients")
+@Data
+@NoArgsConstructor
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
 }
