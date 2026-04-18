32ecab06b5bab49b7daec5f10da6432052a45198
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-17 23:31:59 -0300

    feat: add UserRepository with findByEmail method for authentications

---
+++ src/main/java/management/insight_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package management.insight_api.repository;
+
+import management.insight_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
