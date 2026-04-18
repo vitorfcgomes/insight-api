34a43416afa1bef76e4452a9b045dd29c93340d0
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-17 23:33:16 -0300

    feat: add UserDetailsServiceImpl loading user by email for Spring Security

---
+++ src/main/java/management/insight_api/service/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package management.insight_api.service;
+
+import lombok.RequiredArgsConstructor;
+import management.insight_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+}
