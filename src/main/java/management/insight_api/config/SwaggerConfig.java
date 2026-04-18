7a0b9beb597fbb193348340c8fae76d7943f45f8
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-17 23:37:24 -0300

    feat: add SwaggerConfig with OpenAPI Bearer Auth scheme for JWT authentication

---
+++ src/main/java/management/insight_api/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package management.insight_api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PRS Insight API")
+                        .description("API de gestão e análise de projetos")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Auth"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Auth", new SecurityScheme()
+                                .name("Bearer Auth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
